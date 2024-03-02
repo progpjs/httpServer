9036a43aa70766e284db522c0a0ae35419a8ae7e
johanpiquet <johan.piquet@gmail.com>
2024-03-02 22:53:51 +0100

    Adding proxy functionalities

--- libFastHttpImpl/proxy.go
+++ libFastHttpImpl/proxy.go
@@ -6,21 +6,27 @@ import (
 	"time"
 )
 
-func BuildProxyMiddleware(targetUrl string, timeOutInSec int64) (httpServer.HttpMiddleware, error) {
+// BuildProxyAsIsMiddleware returns a middleware allowing to proxy a request as-is.
+// Here there is not path translation.
+func BuildProxyAsIsMiddleware(targetHostName string, timeOutInSec int64) (httpServer.HttpMiddleware, error) {
 	uri := fasthttp.AcquireURI()
-	err := uri.Parse(nil, []byte(targetUrl))
+	err := uri.Parse(nil, []byte(targetHostName))
 	if err != nil {
+		fasthttp.ReleaseURI(uri)
 		return nil, err
 	}
+	targetHostName = string(uri.Host())
+	fasthttp.ReleaseURI(uri)
 
 	// https://github.com/valyala/fasthttp/blob/7e1fb718543e4e00f807f081b63ba387570690f4/fasthttpproxy/http.go#L38
 	return func(call httpServer.HttpRequest) error {
 		// Here we will reuse the current request.
 		fastCall := call.(*fastHttpRequest).fast
-		//fastCall.Request.SetHost(targetUrl)
+		//fastCall.Request.SetHost(targetHostName)
 
 		callUri := fastCall.Request.URI()
-		uri.CopyTo(callUri)
+		callUri.SetHost(targetHostName)
+		//uri.CopyTo(callUri)
 
 		err := fasthttp.DoTimeout(&fastCall.Request, &fastCall.Response, time.Second*(time.Duration)(timeOutInSec))
 		//err := gFetchHttpClient.Do(&fastCall.Request, resp)
